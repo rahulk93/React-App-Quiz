13f68af3a5bf133f2e0b73aea8c01844e5b900d5
Rahul Khatri <forever_rahul16@hotmail.com>
2020-11-06 15:55:26 +1300

    Updates

---
+++ precache-manifest.19f5eaf79423b5a1a3aac41b0f40265f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5ae24a7d605a63edbcab70a29f7be3d",
+    "url": "/React-App-Quiz/index.html"
+  },
+  {
+    "revision": "f7f4d8e5de5a7ca833e7",
+    "url": "/React-App-Quiz/static/css/main.23c8d00c.chunk.css"
+  },
+  {
+    "revision": "0abbf96aa3b994f72138",
+    "url": "/React-App-Quiz/static/js/2.2bc536dd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App-Quiz/static/js/2.2bc536dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f4d8e5de5a7ca833e7",
+    "url": "/React-App-Quiz/static/js/main.10255050.chunk.js"
+  },
+  {
+    "revision": "24ed510565686f1d3580",
+    "url": "/React-App-Quiz/static/js/runtime-main.0bb1a68e.js"
+  }
+]);
